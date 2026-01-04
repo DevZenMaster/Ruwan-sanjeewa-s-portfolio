507c9d170a341b63c0be420847deb3a5b6905b4e
DevZenMaster <devzen.master@gmail.com>
2026-01-05 04:03:49 +0530

    update linkedin url

--- src/appData/personal.tsx
+++ src/appData/personal.tsx
@@ -2,7 +2,7 @@ import { Facebook, GithubIcon,LinkedIn, X,YouTube} from '@/utils/icons'
 
 export const socials = [
   { href:'https://github.com/DevZenMaster',icon: <GithubIcon /> },
-  { href:'https://www.linkedin.com/in/ruwan-sanjeewa/',icon: <LinkedIn /> },
+  { href:'https://www.linkedin.com/in/ruwansanjeewa/',icon: <LinkedIn /> },
   { href:'https://twitter.com/DevZenMaster',icon: <X /> },
   { href:'https://www.youtube.com/@DevZenMaster',icon: <YouTube /> },
   { href:'https://web.facebook.com/tmruwansanjeewa/',icon: <Facebook /> },
